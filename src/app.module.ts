fa827b65d4e34a1e98b557320883a7cf8ff9fc8d
Wanderson Santos <81826043+wandersonDeve@users.noreply.github.com>
2022-10-14 01:25:13 -0300

    chore: add lib rabbitmq and connect to receive msg

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
+import { RabbitMQModule } from '@golevelup/nestjs-rabbitmq';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    RabbitMQModule.forRoot(RabbitMQModule, {
+      uri: 'amqp://admin:admin@localhost:5672',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,14 @@
+import { RabbitSubscribe } from '@golevelup/nestjs-rabbitmq';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  @RabbitSubscribe({
+    exchange: 'amq.direct',
+    routingKey: 'pagamentos',
+    queue: 'nest-consumer',
+  })
+  getHello(message) {
+    console.log(`${JSON.stringify(message)}`);
   }
 }
